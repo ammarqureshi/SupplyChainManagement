c7665b609a9709fba9a1cf5b16b49c726195dd66
ammar <user@Users-MacBook-Pro.local>
2020-07-11 22:46:02 +0100

    deployed to rinkeby

--- project/truffle.js
+++ project/truffle.js
@@ -1,23 +1,60 @@
 
-const HDWalletProvider = require("truffle-hdwallet-provider");
+
+/**
+ * Use this file to configure your truffle project. It's seeded with some
+ * common settings for different networks and features like migrations,
+ * compilation and testing. Uncomment the ones you need or modify
+ * them to suit your project as necessary.
+ *
+ * More information about configuration can be found at:
+ *
+ * truffleframework.com/docs/advanced/configuration
+ *
+ * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura API
+ * keys are available for free at: infura.io/register
+ *
+ * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. If you're publishing your code to GitHub make sure you load this
+ * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ *
+ */
+
+// const HDWalletProvider = require('truffle-hdwallet-provider');
+var HDWalletProvider = require("@truffle/hdwallet-provider");
+
+// const fs = require('fs');
+const mnemonic = "more pink among attend smoke emotion slim finger copper clog cactus stage";
 
 module.exports = {
+
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*" // Match any network id
-    },
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+     },
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
     rinkeby: {
-      provider: function() {
-     return new HDWalletProvider("candy maple cake sugar pudding cream honey rich smooth crumble sweet treat",
-      "https://rinkeby.infura.io/v3/<Key>")
-         },
-          network_id: '4',
-      gas: 4500000,
-      gasPrice: 10000000000,
-        }
+      // provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/${infuraKey}`),
+      provider : new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/51d9a7bff6ef478e81791999b8b3e336'),
+
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        // gasPrice: 10000000000,
+      confirmations: 0,    // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
   },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
   compilers: {
     solc: {
       version: "^0.6.0", // A version or constraint - Ex. "^0.5.0"
@@ -25,4 +62,4 @@ module.exports = {
       docker: false, // Use a version obtained through docker
     }
   }
-};
+}
